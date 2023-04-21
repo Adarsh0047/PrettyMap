93bb3bd111b1d53d49a69f7e59c20be4b6044077
Adarsh <37295203+Adarsh0047@users.noreply.github.com>
2023-04-21 15:04:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from prettymaps import plot
+from matplotlib.font_manager import FontProperties
+import matplotlib.pyplot as plt
+from PIL import Image
+st.title("Pretty Maps Application")
+
+st.sidebar.title("Input")
+form = st.sidebar.form("my_form")
+place = form.text_area("Enter the place that you want to see the map")
+circle = form.checkbox("Circle")
+rad = form.slider("Radius", 1000, 10000, 2000, 100)
+p_name = form.text_input("Title of figure:")
+submit = form.form_submit_button("Submit")
+if submit:
+    with st.spinner(f'Building a map of {p_name}.. Hold On...'):
+            fig, ax = plt.subplots(figsize = (13, 13))
+            figure = plot(
+                place,
+                circle = circle,
+                radius = rad,
+                ax=ax
+            )
+            fig.patch.set_facecolor('#F2F4CB')
+            ax.set_title(
+            p_name,
+            fontproperties = FontProperties(
+            fname = 'PermanentMarker-Regular.ttf',
+                size = 50
+            )
+            )
+            plt.savefig(f"{p_name}.jpeg", dpi=600)
+            # figure.savefig(f"{p_name}.jpg")
+            figure = Image.open(f"{p_name}.jpeg")
+            st.image(figure)
+            with open(f"{p_name}.jpeg", "rb") as file:
+                st.download_button("Download Map",
+                            data=file,
+                            file_name=f"{p_name}.jpeg",
+                            mime="image/png")
+
+        # except:
+        #     st.warning("The place that you typed does not exist or the format is wrong")
+else:
+    st.info("Enter a place")
